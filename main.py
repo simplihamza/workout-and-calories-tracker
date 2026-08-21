37ce1d12bd3d88f772dc20c554d3124b24fe5ca4
simplihamza <simplihamza@gmail.com>
2026-08-21 15:07:05 +0300

    Initial Commit: Creating a workout and calorie tracker program using some custom API and Sheety API

---
+++ main.py
@@ -0,0 +1,47 @@
+import requests
+from datetime import datetime
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+APP_ID = os.environ.get("APP_ID")
+API_KEY = os.environ.get("API_KEY")
+GENDER = os.environ.get("GENDER")
+WEIGHT_KG = int(os.environ.get("WEIGHT_KG"))
+HEIGHT_CM = int(os.environ.get("HEIGHT_CM"))
+AGE = int(os.environ.get("AGE"))
+
+nutrition_and_exercise_endpoint = "https://app.100daysofpython.dev/v1/nutrition/natural/exercise"
+workout_endpoint = "https://api.sheety.co/b31e3f1dcf8d6c6205b00bd0ba346c69/manoWorkouts/workouts"
+
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY
+}
+
+exercise = input("Enter what activity you performed: ")
+
+parameters = {
+    "query": exercise,
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
+}
+
+response = requests.post(nutrition_and_exercise_endpoint, headers=headers, json=parameters)
+result = response.json()
+
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+
+for exercise in result["exercises"]:
+    sheet_params = {
+        "Exercise": exercise,
+        "Date": today_date,
+        "Time": now_time,
+        "Duration": exercise["duration_min"],
+        "Calories": exercise["nf_calories"]
+    }
+sheet_response = requests.post(workout_endpoint, json=sheet_params)
+print(sheet_response.text)
